126e797dd14f8c991703254409d46feccf7e8526
Ariba-Jawaid721 <javeedsania124@gmail.com>
2026-09-05 15:50:20 +0500

    Fix server port configuration

--- backend/server.js
+++ backend/server.js
@@ -105,4 +105,6 @@ if (require.main === module) {
   startServer();
 }
 
-module.exports = { app, startServer };
+module.exports = app;
+module.exports.app = app;
+module.exports.startServer = startServer;
